ee4641878e4138db4e619934e31f068cb984e33b
mi4en <marionikolov87@gmail.com>
2018-12-16 19:58:20 +0200

    add app.js; add models; add middleware; add main css;

---
+++ models/dog.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose')
+
+// Define schema
+var dogSchema = new mongoose.Schema({
+  name: String,
+  breed: String,
+  image: String,
+  imageId: String,
+  description: String,
+  createdAt: { type: Date, default: Date.now },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    username: String
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
+})
+
+// Define and export model to be used from app.js
+module.exports = mongoose.model('Dog', dogSchema)
